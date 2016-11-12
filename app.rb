7235ebdf6ae8169cfeade38f2367bb137990aa3d
Alexey Tsverov <targence@gmail.com>
2016-11-13 01:56:37 +0300

    rm signature for post /

--- app.rb
+++ app.rb
@@ -34,82 +34,70 @@ end
 
 post "/" do
 
-  if params[:signature]
-    array = [ENV['APP_TOKEN'], params[:timestamp], params[:nonce]].sort
-    if params[:signature] == Digest::SHA1.hexdigest(array.join)
-      params[:echostr]
-    else
-      status 418
-      body "Forbidden"
-    end
-  else
+  @message = MessageParser.parse(request.body.read)
 
-    @message = MessageParser.parse(request.body.read)
+  if current_user && current_user.admin?
 
-    if current_user && current_user.admin?
+    case @message.options[:content]
+    when /^users.*/i
+      all_users = User.all
+      @sending_text = "id   name\n\n" + all_users.map{|u| "#{u.id}. #{u.name}" }.join("\n") + "\n\ntotal users: #{all_users.count}"
+    when /^adduser\s\S+\s\S+/i
+      openid = (@message.options[:content].match /(\w*)\s(\S+)\s(\S+)/)[2]
+      name = (@message.options[:content].match /(\w*)\s(\S+)\s(\S+)/)[3]
+      new_user = User.new(openid: openid, name: name)
 
-      case @message.options[:content]
-      when /^users.*/i
-        all_users = User.all
-        @sending_text = "id   name\n\n" + all_users.map{|u| "#{u.id}. #{u.name}" }.join("\n") + "\n\ntotal users: #{all_users.count}"
-      when /^adduser\s\S+\s\S+/i
-        openid = (@message.options[:content].match /(\w*)\s(\S+)\s(\S+)/)[2]
-        name = (@message.options[:content].match /(\w*)\s(\S+)\s(\S+)/)[3]
-        new_user = User.new(openid: openid, name: name)
-
-       if User.find_by_openid(openid)
-         @sending_text = "😧 user already exists"
-       elsif new_user.valid?
-         new_user.save
-         @sending_text = "👌 user has added"
-        else
-          @sending_text = "😧"
-        end
-      when /^deluser\s\d+/i
-        id = (@message.options[:content].match /(\w*)\s(\d+)/)[2]
-        user = User.find(id.to_i)
-        if user.nil?
-          @sending_text = "😧 user doesn't exist"
-        elsif !user.admin? || current_user.type == 'Users::SuperUser'
-          user.destroy
-          @sending_text = "👌 user deleted"
-        else
-          @sending_text = "😧 can't delete admin"
-        end
-      when /^help.*/i
-        @sending_text = "COMMANDS\n\nNOT MEMBERS:\n• membership - how to become a member\n\nMEMBERS:\n• key - get a key for the doorlock\n\nADMINS:\n• users - list all users\n• adduser - add a new user\n 👉 example: adduser o8POWszdG1T0ZEVYv5qWrZxO0BAM 肖红\n• deluser - delete a user\n 👉 example: deluser 1"
+     if User.find_by_openid(openid)
+       @sending_text = "😧 user already exists"
+     elsif new_user.valid?
+       new_user.save
+       @sending_text = "👌 user has added"
       else
-        @sending_text = "💩 听不懂"
+        @sending_text = "😧"
       end
+    when /^deluser\s\d+/i
+      id = (@message.options[:content].match /(\w*)\s(\d+)/)[2]
+      user = User.find(id.to_i)
+      if user.nil?
+        @sending_text = "😧 user doesn't exist"
+      elsif !user.admin? || current_user.type == 'Users::SuperUser'
+        user.destroy
+        @sending_text = "👌 user deleted"
+      else
+        @sending_text = "😧 can't delete admin"
+      end
+    when /^help.*/i
+      @sending_text = "COMMANDS\n\nNOT MEMBERS:\n• membership - how to become a member\n\nMEMBERS:\n• key - get a key for the doorlock\n\nADMINS:\n• users - list all users\n• adduser - add a new user\n 👉 example: adduser o8POWszdG1T0ZEVYv5qWrZxO0BAM 肖红\n• deluser - delete a user\n 👉 example: deluser 1"
+    else
+      @sending_text = "💩 听不懂"
+    end
 
-      elsif current_user && current_user.member?
-
-        case @message.options[:content]
-        when /^membership.*/i
-          @sending_text = "This is your wechat id ☝️\n\n#{@message.options[:to_user]}\n\nPlease copy it and send to administrator"
-        when /^key.*/i
-          totp = ROTP::TOTP.new(ENV['TOTP_TOKEN'])
-          @sending_text = "🔑 " + totp.at(Time.now + 5) + "#"
-        when /^help.*/i
-          @sending_text = "COMMANDS\n\nNOT MEMBERS:\n• membership - how to become a member\n\nMEMBERS:\n• key - get a key for the doorlock"
-        else
-          @sending_text = "💩 听不懂"
-        end
-
-    else # not members
+    elsif current_user && current_user.member?
 
       case @message.options[:content]
       when /^membership.*/i
         @sending_text = "This is your wechat id ☝️\n\n#{@message.options[:to_user]}\n\nPlease copy it and send to administrator"
+      when /^key.*/i
+        totp = ROTP::TOTP.new(ENV['TOTP_TOKEN'])
+        @sending_text = "🔑 " + totp.at(Time.now + 5) + "#"
       when /^help.*/i
         @sending_text = "COMMANDS\n\nNOT MEMBERS:\n• membership - how to become a member\n\nMEMBERS:\n• key - get a key for the doorlock"
       else
         @sending_text = "💩 听不懂"
       end
+
+  else # not members
+
+    case @message.options[:content]
+    when /^membership.*/i
+      @sending_text = "This is your wechat id ☝️\n\n#{@message.options[:to_user]}\n\nPlease copy it and send to administrator"
+    when /^help.*/i
+      @sending_text = "COMMANDS\n\nNOT MEMBERS:\n• membership - how to become a member\n\nMEMBERS:\n• key - get a key for the doorlock"
+    else
+      @sending_text = "💩 听不懂"
     end
-
-    erb :text, :format => :xml
-
   end
+
+  erb :text, :format => :xml
 
 end
